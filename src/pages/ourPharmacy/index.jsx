01953c36ba1637050006c60571d480b5efff63b1
kev-69 <bismarkobuobi19@gmail.com>
2024-07-12 17:37:25 +0000

    progress on pharmacy page

--- src/pages/ourPharmacy/index.jsx
+++ src/pages/ourPharmacy/index.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
 
+// styles import
+import "./index.css"
+
 const OurPharmacy = () => {
   return (
-    <div>OurPharmacy</div>
+    <div className='main-div'>
+      <div className="pharmacy-header">
+        Our Pharmacy
+      </div>
+
+      <div className="category-drugs-main">
+        <div className="search-filter">
+          <div className='search'>
+            <input type="text" placeholder='Search' />
+          </div>
+          <div className='filter'>
+            <span>Filter</span>
+          </div>
+        </div>
+
+        {/* List of categories */}
+        <div className='category-drug'>
+          <div className='category'>
+            <h2>Categories</h2>
+            <ul>
+              <li>Antibiotics </li>
+              <li>Dewormers</li>
+              <li>Stomach Upsets</li>
+              <li>Multivitamins</li>
+              <li>Blood Tonics</li>
+              <li>Malaria Drugs</li>
+              <li>Herbal Drugs</li>
+              <li>Cough and Cold Syrups</li>
+            </ul>
+          </div>
+
+          {/* The drug under the category clicked should appear here */}
+          <div className="drugs">
+
+          </div>
+        </div>
+
+        {/* Recommended drugs and products */}
+        <div className="recommended">
+          <h1>Recommend Drugs</h1>
+
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
