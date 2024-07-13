39c193793eb6a96cc7cb3cf7d5999b906c6fae9d
kev-69 <bismarkobuobi19@gmail.com>
2024-07-13 11:20:06 +0000

    Drug categories

---
+++ src/pages/ourPharmacy/Categories.jsx
@@ -0,0 +1,36 @@
+const Data = [
+    {
+        id: 0,
+        category: "Antibiotics",
+    },
+    {
+        id: 1,
+        category: "Dewormers",
+    },
+    {
+        id: 2,
+        category: "Stomach Upsets",
+    },
+    {
+        id: 3,
+        category: "Multivitamins",
+    },
+    {
+        id: 4,
+        category: "Blood Tonics",
+    },
+    {
+        id: 5,
+        category: "Malaria Drugs",
+    },
+    {
+        id: 6,
+        category: "Cough And Cold Syrups",
+    },
+    {
+        id: 7,
+        category: "Herbal Drugs",
+    },
+];
+
+export default Data;
