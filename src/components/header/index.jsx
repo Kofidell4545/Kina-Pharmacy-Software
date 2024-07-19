4145c76cdf224b17a6e19f22e28d80c07c78e7db
kev-69 <bismarkobuobi19@gmail.com>
2024-07-19 16:41:03 +0000

    Logo import correction

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,7 +5,7 @@ import { Link, NavLink } from 'react-router-dom';
 import "./styles.css";
 
 // Logo import
-import logo from "../../assets/logo.png";
+import logo from "../../assets/utils/logo.png";
 
 
 const Navbar = () => {
