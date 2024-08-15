791d28975b5367a5777c34c117b8352ea5dff303
kev-69 <bismarkobuobi19@gmail.com>
2024-08-15 20:14:47 +0000

    Drugs information

--- kina-pharmacy-api/server.js
+++ kina-pharmacy-api/server.js
@@ -5,21 +5,6 @@ const port = 3000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// Example data
-const drugs = [
-  {
-    id: 1,
-    category: 'Antibiotics',
-    drugName: 'Amoxicillin',
-    description: 'An antibiotic used to treat bacterial infections.',
-    uses: ['Treat bacterial infections', 'Reduce symptoms'],
-    keyComponents: ['Amoxicillin trihydrate'],
-    price: '$10',
-    inStock: true,
-    images: ['image1.jpg', 'image2.jpg', 'image3.jpg']
-  },
-  // Add more drug objects here
-];
 
 // Routes
 app.get('/api/drugs', (req, res) => {
