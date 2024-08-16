91580f8070f95d914eca3f1d25635f371b49532f
kev-69 <bismarkobuobi19@gmail.com>
2024-08-16 00:06:35 +0000

    Updates on server file

--- kina-pharmacy-api/server.js
+++ kina-pharmacy-api/server.js
@@ -7,18 +7,18 @@ const herbalDrugsData = require("./Drugs/HDrugs");
 const malariaDrugsData = require("./Drugs/MDrugs");
 const multivitaminsData = require("./Drugs/Multivitamins");
 const stomachUpsetsData = require("./Drugs/SUpsets");
-
-// Importing the files to the server
-const antibioticsData = require("./Drugs/AntibioticsS");
+const antibioticsData = require("./Drugs/Antibiotics");
 const bloodTonicsData = require("./Drugs/BTonics");
 const coughSyrupsData = require("./Drugs/CCSyrups");
 const dewormersData = require("./Drugs/Dewormers");
+
 // Middleware to parse JSON bodies
 app.use(express.json());
 
 // Drug Routes
 app.get("/api/drugs", (req, res) => {
-    const allDrugs = [ ...herbalDrugsData, ...malariaDrugsData, ...multivitaminsData, stomachUpsetsData ]
+    const allDrugs = [ ...herbalDrugsData, ...malariaDrugsData, ...multivitaminsData, stomachUpsetsData, ...antibioticsData, ...bloodTonicsData, ...coughSyrupsData, ...dewormersData ]
+
     res.json(allDrugs);
 });
 
@@ -26,7 +26,7 @@ app.get("/api/drugs", (req, res) => {
 // Routes to get drugs by their id
 app.get("/api/drugs/:id", (req, res) => {
     const drugId = parseInt(req.params.id);
-    const allDrugs = [...herbalDrugsData, ...malariaDrugsData, ...multivitaminsData, stomachUpsetsData];
+    const allDrugs = [...herbalDrugsData, ...malariaDrugsData, ...multivitaminsData, stomachUpsetsData, ...antibioticsData, ...bloodTonicsData, ...coughSyrupsData, ...dewormersData];
     const drug = allDrugs.find((d) => d.id === drugId);
     if (drug) {
       res.json(drug);
