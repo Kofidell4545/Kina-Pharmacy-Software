3edd82ae5558b1bbbfdf9b82de888a3efa1155e2
kev-69 <bismarkobuobi19@gmail.com>
2024-08-16 00:51:30 +0000

    cors package added

--- kina-pharmacy-api/server.js
+++ kina-pharmacy-api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const port = 3000;
 
 // Importing drugs data from categories
@@ -13,26 +14,44 @@ const coughSyrupsData = require("./Drugs/CCSyrups");
 const dewormersData = require("./Drugs/Dewormers");
 
 // Middleware to parse JSON bodies
-app.use(express.json());
+app.use(cors());
+
 
 // Drug Routes
 app.get("/api/drugs", (req, res) => {
-    const allDrugs = [ ...herbalDrugsData, ...malariaDrugsData, ...multivitaminsData, stomachUpsetsData, ...antibioticsData, ...bloodTonicsData, ...coughSyrupsData, ...dewormersData ]
+  const allDrugs = [
+    ...herbalDrugsData,
+    ...malariaDrugsData,
+    ...multivitaminsData,
+    ...stomachUpsetsData, // Add the spread operator here
+    ...antibioticsData,
+    ...bloodTonicsData,
+    ...coughSyrupsData,
+    ...dewormersData,
+  ];
 
-    res.json(allDrugs);
+  res.json(allDrugs);
 });
-
 
 // Routes to get drugs by their id
 app.get("/api/drugs/:id", (req, res) => {
-    const drugId = parseInt(req.params.id);
-    const allDrugs = [...herbalDrugsData, ...malariaDrugsData, ...multivitaminsData, stomachUpsetsData, ...antibioticsData, ...bloodTonicsData, ...coughSyrupsData, ...dewormersData];
-    const drug = allDrugs.find((d) => d.id === drugId);
-    if (drug) {
-      res.json(drug);
-    } else {
-      res.status(404).json({ message: "Drug not found" });
-    }
+  const drugId = parseInt(req.params.id);
+  const allDrugs = [
+    ...herbalDrugsData,
+    ...malariaDrugsData,
+    ...multivitaminsData,
+    ...stomachUpsetsData, // Add the spread operator here
+    ...antibioticsData,
+    ...bloodTonicsData,
+    ...coughSyrupsData,
+    ...dewormersData,
+  ];
+  const drug = allDrugs.find((d) => d.id === drugId);
+  if (drug) {
+    res.json(drug);
+  } else {
+    res.status(404).json({ message: "Drug not found" });
+  }
 });
 
 app.listen(port, () => {
